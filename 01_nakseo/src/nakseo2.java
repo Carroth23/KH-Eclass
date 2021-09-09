3f157ca89b6e2fee72554106655cba6c795df8f4
Carroth23 <wlsrb230@gmail.com>
2021-09-09 17:52:20 +0900

    Day_05_Off2

---
+++ 01_nakseo/src/nakseo2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class nakseo2 {
+	public static void main(String[] args) {
+
+		
+		
+	}
+}
---
+++ Day_06_Practice/src/Exam_04_random.java
@@ -0,0 +1,19 @@
+
+public class Exam_04_random {
+	public static void main(String[] args) {
+		
+		// 난수 - Random Number
+		
+		double rand = Math.random();
+		// Math.random 으로 발생하는 난수는 0 ~ 1사이의 double값으로 발생함.(0이 나오지않고 1이 나오지않는다)
+		System.out.println(rand); // 예상불가능한 값
+		
+		// 16 ~ 34 사이의 난수
+		
+//		난수 값 구하는 공식
+//		최소값 : X
+//		최대값 : Y
+//		((int)(Math.random() * (Y - X + 1) + X))
+		
+	}
+}
