e255e65790ff8b810061c7ddabcfd6778c604243
Carroth23 <wlsrb230@gmail.com>
2021-09-15 01:34:59 +0900

    night_Off

---
+++ Day_09_Method/src/Exam_02.java
@@ -0,0 +1,27 @@
+
+public class Exam_02 {
+
+//	double random() {
+//		// 난수 생성 코드
+//		// 반환
+//	}
+//	
+//	String nextLine() {
+//		// 버퍼를 확인하고 비어있으면 입력받는 기능, 안비어있으면 데이터를 가져오는 기능
+//		// 반환
+//	}
+//	
+//	int parseInt(String str){
+//		
+//	}
+//	
+//	public static void main(String[] args) {
+//		double rand = Math.random();
+//		String msg = sc.nextLine();
+//		
+//		int kor = Integer.parseInt(sc.nextLine(/*여기의 리턴 타입은 String*/));
+//		//nextLine()는 String를 리턴
+//		
+//	}
+
+}
