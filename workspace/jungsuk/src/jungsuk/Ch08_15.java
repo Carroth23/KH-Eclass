4793e850c032c671d0232f1fc45ada67c7b12c33
Carroth23 <wlsrb230@gmail.com>
2022-01-01 23:48:11 +0900

    Exception-Re

---
+++ workspace/jungsuk/src/jungsuk/Ch08_15.java
@@ -0,0 +1,16 @@
+package jungsuk;
+
+public class Ch08_15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			// 예외가 발생할것같은 코드
+		} catch(Exception e ) {
+			// 예외가 발생하면 처리될 코드
+		}finally {
+			// 예외가 발생하던 말던 처리될 코드
+		}
+	}
+
+}
