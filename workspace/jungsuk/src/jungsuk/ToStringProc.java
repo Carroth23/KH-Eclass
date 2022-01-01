72042d4031a2f7e538e9154394e325734b16b8d9
Carroth23 <wlsrb230@gmail.com>
2022-01-02 02:23:29 +0900

    커밋테스트

--- workspace/jungsuk/src/jungsuk/ToStringProc.java
+++ workspace/jungsuk/src/jungsuk/ToStringProc.java
@@ -12,6 +12,8 @@ class Proc {
 	public int x = 10;
 	public String y = "하이";
 	
+	// Object클래스의 toString()을 오버라이딩
+	// 객체를 문자열로 바꾸는것 = IV값들을 문자열로 바꾸는듯
 	public String toString() {
 		return "x : " + x + ", y : " + y;
 	}
