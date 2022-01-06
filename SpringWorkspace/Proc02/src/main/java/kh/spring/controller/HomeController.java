7438ec72e1198fd131f14cbe19228047239b0239
Carroth23 <wlsrb230@gmail.com>
2022-01-06 22:31:54 +0900

    카카오로그인 시도중

--- SpringWorkspace/Proc02/src/main/java/kh/spring/controller/HomeController.java
+++ SpringWorkspace/Proc02/src/main/java/kh/spring/controller/HomeController.java
@@ -1,15 +1,37 @@
 package kh.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
+	private String url = "https://kauth.kakao.com/oauth/authorize?client_id=b7b0a7f6722957ddef971b2ff4061bd7&redirect_uri=http://localhost/member/login&response_type=code";
 
+	
 	@RequestMapping("/")
 	public String home() {
 		return "home";
+	}
+	
+	@RequestMapping("login")
+	public String login(String code, String error) {
+		System.out.println("반환된 코드 : " + code);
+		System.out.println("에러 코드 : " + error);
+		System.out.println("로그인 성공?");
+		return "/member/loginsu";
+	}
+	
+	
+	
+	
+	
+	@ExceptionHandler
+	public String ExceptionHandler(Exception e) {
+		e.printStackTrace();
+		e.getMessage();
+		return "redirect:/";
 	}
 	
 }
---
+++ SpringWorkspace/Proc03/src/main/java/kh/spring/interceptor/Interceptor.java
@@ -0,0 +1,21 @@
+package kh.spring.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class Interceptor implements HandlerInterceptor {
+	
+	@Autowired
+	private HttpSession session;
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+}
