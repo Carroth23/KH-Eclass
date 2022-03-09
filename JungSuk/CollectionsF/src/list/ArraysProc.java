7fc83cb21ee6c24f45e8e7c8c68530223444dd04
Carroth23 <wlsrb230@gmail.com>
2022-03-09 17:55:49 +0900

    Array

--- JungSuk/CollectionsF/src/list/ArraysProc.java
+++ JungSuk/CollectionsF/src/list/ArraysProc.java
@@ -1,12 +1,25 @@
 package list;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ArraysProc {
 	public static void main(String[] args) {
+		// ArrayList는 동기화처리가 안되어있다.
+		// Vector은 동기화처리되어있음
 		int[] arr = {0,1,2,3,4};
 		
 		int[] arr2 = Arrays.copyOf(arr, arr.length);
+		ArrayList list = new ArrayList();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		System.out.println(list.size());
+		list.remove(new Integer(5));
+		System.out.println(list.size());
 		
+		// 리스트 지울땐 되도록 뒤에서부터 지워야댐
 	}
 }
--- JungSuk/Jungsuk01/src/strings/Ex09_11.java
+++ JungSuk/Jungsuk01/src/strings/Ex09_11.java
@@ -13,6 +13,12 @@ public class Ex09_11 {
 		String s2 = sb2.toString();
 		System.out.println("s.equals(s2) ? " + s.equals(s2));
 		System.out.println("s == s2 ? " + (s == s2));
+		
+		// 배열은 길이 변경 불가.
+		// 배열의 공간이 부족하면 새로 생성하고 복사하고... 이러면 성능상의 문제가 생김.
+		// StringBuffer는 저장할 문자열의 길이를 고려해서 적절한 크기로 생성
+		// StringBuffere의 기본 크기는 16 (별 의미는 없음)
+		// StringBuffere의 메서드는 자기 자신을 반환
 	}
 
 }
